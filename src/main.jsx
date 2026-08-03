d5d2f64420d6976ac70e6d2d1c68780e1e2ffa3e
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-08-03 09:23:51 +0530

    feat: add global glass theme styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./styles/globals.css";
 import "./index.css";
+import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
